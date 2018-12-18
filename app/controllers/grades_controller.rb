5a4f5bed93f0bb7a87c271a0e479e362208f5adb
devlucasrocha <lucasgeorgerocha@gmail.com>
2018-12-18 16:14:20 -0200

    Usando o devise para proteger o model grade

--- app/controllers/grades_controller.rb
+++ app/controllers/grades_controller.rb
@@ -1,4 +1,5 @@
 class GradesController < ApplicationController
+  before_action :authenticate_professor!
   before_action :set_grade, only: [:show, :edit, :update, :destroy]
   
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  namespace :site do
+    get 'welcome/index'
+  end
   devise_for :professors
-  get 'welcome/index'
+  get 'site/welcome/index'
   resources :grades
-  root to: "welcome#index"
+  root to: "site/welcome#index"
+  get "home", to: "site/welcome#index"
 end
